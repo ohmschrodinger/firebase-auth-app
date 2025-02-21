66ed0be61c1dede65a14c8e369590ee471e88726
ohmschrodinger <omdhamame@gmail.com>
2025-02-21 19:53:36 +0530

    used link instead of navigate

--- src/pages/Signin.jsx
+++ src/pages/Signin.jsx
@@ -1,41 +1,33 @@
-import React, { useEffect } from 'react';
-import GoogleButton from 'react-google-button';
+import React from 'react';
+import { Link } from 'react-router-dom';
 import { UserAuth } from '../context/AuthContext';
 import { useNavigate } from 'react-router-dom';
-// import './Signin.css'; // optional for separate styling
 
 const Signin = () => {
-  const { googleSignIn, user } = UserAuth();
+  const { googleSignIn } = UserAuth();
   const navigate = useNavigate();
 
   const handleGoogleSignIn = async () => {
     try {
+      // This now uses signInWithPopup in your AuthContext (make sure to update it there accordingly)
       await googleSignIn();
+      <Link to="/Home" className="link">
+      Sign in
+    </Link>
+      // navigate('/src/pages/Account.jsx');
     } catch (error) {
-      console.prompt("whatttttt");
       console.log(error);
     }
   };
-
-  useEffect(() => {
-    if (user != null) {
-      navigate('/account');
-    }
-  }, [user, navigate]);
 
   return (
     <div className="signin-container">
       <h1 className="signin-title">Sign in</h1>
       <div className="signin-button-wrapper">
-      {/* <GoogleButton onClick={handleGoogleSignIn} /> */}
         <button onClick={handleGoogleSignIn}>Sign in with Google</button>
-
       </div>
     </div>
   );
 };
 
 export default Signin;
-
-
-
